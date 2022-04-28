dae0bf7aeea59424783ec045a0928b3139b77953
Jordi Jiménez Coves <jjimenezcoves@gmail.com>
2022-04-28 19:07:45 +0200

    Primer commit

---
+++ controllers/usuarios.js
@@ -0,0 +1,53 @@
+const { response, request } = require('express');
+
+const usuariosGet = (req = request, res = response) => {
+
+    const { nombre, edad } = req.query;
+
+    res.json({
+        msg: 'get API - controlador',
+        nombre,
+        edad
+    });
+}
+
+const usuariosPost = (req, res = response) => {
+
+    const { nombre, edad } = req.body;
+
+    res.json({
+        msg: 'post API - controlador',
+        nombre,
+        edad
+    });
+}
+
+const usuariosPut = (req, res = response) => {
+
+    const { id } = req.params;
+
+    res.json({
+        msg: 'put API - controlador',
+        id
+    });
+}
+
+const usuariosPatch = (req, res = response) => {
+    res.json({
+        msg: 'patch API - controlador'
+    });
+}
+
+const usuariosDelete = (req, res = response) => {
+    res.json({
+        msg: 'delete API - controlador'
+    });
+}
+
+module.exports = {
+    usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    usuariosPatch,
+    usuariosDelete
+}
